58f9a89901b3890dead2b5d9ceab53da5831de03
Ajay fives digital <you@example.com>
2025-01-02 18:07:44 +0530

    config updated

--- config/db.js
+++ config/db.js
@@ -3,21 +3,30 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
-console.log('Environment Variables:', {
-    HOST: process.env.HOST,
-    USERNAME: process.env.MYUSERNAME,
-    PASSWORD: process.env.PASSWORD,
-    DATABASE: process.env.DATABASE,
-});
+    // console.log('Environment Variables:', {
+    //     HOST: process.env.HOST,
+    //     USERNAME: process.env.MYUSERNAME,
+    //     PASSWORD: process.env.PASSWORD,
+    //     DATABASE: process.env.DATABASE,
+    // });
 
 
+// const db = mysql.createPool({
+//     host: process.env.HOST,  // Should match .env key: HOST
+//     user: process.env.MYUSERNAME, // Should match .env key: USERNAME
+//     password: process.env.PASSWORD, // Should match .env key: PASSWORD
+//     database: process.env.DATABASE, // Should match .env key: DATABASE
+// });
+
+
+// Create a MySQL connection pool
 const db = mysql.createPool({
-    host: process.env.HOST,  // Should match .env key: HOST
-    user: process.env.MYUSERNAME, // Should match .env key: USERNAME
-    password: process.env.PASSWORD, // Should match .env key: PASSWORD
-    database: process.env.DATABASE, // Should match .env key: DATABASE
+    host: 'mysql-3be60ee0-ajaysharmawork80-nlpro.f.aivencloud.com',
+    user: 'avnadmin',
+    password: 'AVNS_Z6tbzUKogJuREW-791f',  // Use your MySQL password here if necessary
+    database: 'defaultdb',
+    port: '17489'  // Your database name
 });
-
 
 
 // Export the connection pool
