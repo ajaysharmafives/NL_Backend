5ec045778d39a4a5b42e6fc0462619f538d0b996
Ajay fives digital <you@example.com>
2025-01-04 19:27:13 +0530

    config updated

--- config/db.js
+++ config/db.js
@@ -11,22 +11,22 @@ dotenv.config();
     // });
 
 
-const db = mysql.createPool({
-    host: process.env.HOST,  // Should match .env key: HOST
-    user: process.env.MYUSERNAME, // Should match .env key: USERNAME
-    password: process.env.PASSWORD, // Should match .env key: PASSWORD
-    database: process.env.DATABASE, // Should match .env key: DATABASE
-});
-
-
-// Create a MySQL connection pool
 // const db = mysql.createPool({
-//     host: 'mysql-3be60ee0-ajaysharmawork80-nlpro.f.aivencloud.com',
-//     user: 'avnadmin',
-//     password: 'AVNS_Z6tbzUKogJuREW-791f',  // Use your MySQL password here if necessary
-//     database: 'defaultdb',
-//     port: '17489'  // Your database name
+//     host: process.env.HOST,  // Should match .env key: HOST
+//     user: process.env.MYUSERNAME, // Should match .env key: USERNAME
+//     password: process.env.PASSWORD, // Should match .env key: PASSWORD
+//     database: process.env.DATABASE, // Should match .env key: DATABASE
 // });
+
+
+Create a MySQL connection pool
+const db = mysql.createPool({
+    host: 'mysql-3be60ee0-ajaysharmawork80-nlpro.f.aivencloud.com',
+    user: 'avnadmin',
+    password: 'AVNS_Z6tbzUKogJuREW-791f',  // Use your MySQL password here if necessary
+    database: 'defaultdb',
+    port: '17489'  // Your database name
+});
 
 
 // Export the connection pool
